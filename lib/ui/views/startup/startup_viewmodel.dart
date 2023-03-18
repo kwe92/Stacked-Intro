5402bc207087efcce315f0f3ea47c4e124998192
Kweayon  Clark <Kweayon@gmail.com>
2023-03-17 22:23:33 -0500

    Update

--- lib/ui/views/startup/startup_viewmodel.dart
+++ lib/ui/views/startup/startup_viewmodel.dart
@@ -1,18 +1,29 @@
 import 'package:stacked/stacked.dart';
 import 'package:stacked_counter_app/app/app.locator.dart';
 import 'package:stacked_counter_app/app/app.router.dart';
+import 'package:stacked_counter_app/services/authentication_service.dart';
 import 'package:stacked_services/stacked_services.dart';
 
 class StartupViewModel extends BaseViewModel {
   final _navigationService = locator<NavigationService>();
 
+  final _authenticationService = locator<AuthenticationService>();
+
   // Place anything here that needs to happen before we get into the application
   Future runStartupLogic() async {
     await Future.delayed(const Duration(seconds: 3));
+    _navigationService.navigateToTextFormView();
+    // _authenticationService.userLoggedIn(val: true)
+    //     ? _counterView()
+    //     : _homeView();
 
     // This is where you can make decisions on where your app should navigate when
     // you have custom startup logic
-
-    _navigationService.replaceWithCounterView();
   }
 }
+
+// final _navigationService = locator<NavigationService>();
+
+// void _counterView() => _navigationService.replaceWith(Routes.counterView);
+
+// void _homeView() => _navigationService.replaceWithHomeView();
