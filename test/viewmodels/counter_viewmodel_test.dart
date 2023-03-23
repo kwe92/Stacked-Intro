958d2ba5139d68812471cd7ae3b7e13ed4e194b4
Kwe <Kweayon@gmail.com>
2023-03-23 13:04:50 -0500

    Added COunter View Tests

--- test/viewmodels/counter_viewmodel_test.dart
+++ test/viewmodels/counter_viewmodel_test.dart
@@ -1,11 +1,29 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:stacked_counter_app/app/app.locator.dart';
+import 'package:stacked_counter_app/ui/views/counter/counter_viewmodel.dart';
 
 import '../helpers/test_helpers.dart';
 
 void main() {
   group('CounterViewModel Tests -', () {
+    final counterModel = CounterViewModel();
     setUp(() => registerServices());
+    test('Inital Value: O', () => expect(counterModel.count, 0));
+    test('Increment count: + 1', () {
+      counterModel.increment();
+      expect(counterModel.count, 1);
+    });
     tearDown(() => locator.reset());
+
+    test('Decrement count: - 1', () {
+      counterModel.decrement();
+      expect(counterModel.count, 0);
+    });
+
+    test('If count 0 Decrement does nothing', () {
+      expect(counterModel.count, 0);
+      counterModel.decrement();
+      expect(counterModel.count, 0);
+    });
   });
 }
